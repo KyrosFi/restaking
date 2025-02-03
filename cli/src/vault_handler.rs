0770bd7d76f75143b07382a13813d44c63deb036
murmky <184486394+murmky@users.noreply.github.com>
2025-02-03 20:41:25 +0100

    Fix borrow

--- cli/src/vault_handler.rs
+++ cli/src/vault_handler.rs
@@ -1026,7 +1026,7 @@ impl VaultCliHandler {
         ix_builder
             .config(Config::find_program_address(&self.vault_program_id).0)
             .vault(vault)
-            .delegate_asset_admin(&keypair.pubkey())
+            .delegate_asset_admin(keypair.pubkey())
             .token_mint(token_mint)
             .token_account(token_account)
             .delegate(delegate)
